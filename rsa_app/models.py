1a8e330f90af20eb759f66760d11e7e5f45bf88d
MbengueAissatou <astoumbengue083@gmail.com>
2026-03-04 19:30:06 +0100

    ajout champ name au modele RSAKey

--- rsa_app/models.py
+++ rsa_app/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class RSAKey(models.Model):
-    # your fields here
+    name = models.CharField(max_length=255)  # ← ajouter ce champ
     public_key = models.TextField()
     private_key = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
