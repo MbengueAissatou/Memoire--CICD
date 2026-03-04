704c3ff11deccdced474cb1f590dab765f1598c7
MbengueAissatou <astoumbengue083@gmail.com>
2026-03-05 00:52:18 +0100

    ajout

--- rsa_app/models.py
+++ rsa_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class RSAKey(models.Model):
-    name = models.CharField(max_length=255)  # ← ajouter ce champ
+    name = models.CharField(max_length=255)  
     public_key = models.TextField()
     private_key = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
